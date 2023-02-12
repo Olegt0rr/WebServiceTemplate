6fa3130b049b8c215260c49c53c552806d640b2c
Oleg A <t0rr@mail.ru>
2023-02-12 20:38:27 +0300

    feat: add k8s

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from aiohttp.web_app import Application
 
-from app.telegram.setup import setup_telegram
+from .core.handlers import setup_routes
+from .telegram.setup import setup_telegram
 
 
 def app_factory() -> Application:
     """Create web app."""
     app = Application()
+
+    setup_routes(app)
     setup_telegram(app)
+
     return app
---
+++ app/core/handlers/__init__.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+
+from . import k8s
+
+if TYPE_CHECKING:
+    from aiohttp.web_app import Application
+
+
+def setup_routes(app: Application) -> None:
+    """Register handlers."""
+    k8s.setup(app)
