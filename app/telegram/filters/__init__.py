f1336623357dadc410cdbd5a5bae35cc5bae4a2d
Oleg A <t0rr@mail.ru>
2023-02-13 23:53:01 +0300

    feat: add chat_type filter

---
+++ app/telegram/filters/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "ChatType",
+]
+
+from .chat_type import ChatType
---
+++ app/telegram/filters/chat_type.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING
+
+from aiogram.filters import BaseFilter
+
+if TYPE_CHECKING:
+    from typing import Set, Union
+
+    from aiogram.types import Chat, TelegramObject
+
+
+class ChatType(BaseFilter):
+    """Represents chat type filter."""
+
+    def __init__(self, chat_type: "Union[str, Set[str]]") -> None:
+        self._check_type(chat_type)
+        self.chat_type = chat_type
+
+    async def __call__(self, object_: "TelegramObject") -> bool:
+        """Apply filter."""
+        chat = self._get_chat(object_)
+        if isinstance(self.chat_type, str):
+            return chat.type == self.chat_type
+        return chat.type in self.chat_type
+
+    @staticmethod
+    def _get_chat(object_: "TelegramObject") -> "Chat":
+        """Get chat from Telegram object."""
+        if hasattr(object_, "chat"):
+            return object_.chat
+
+        if hasattr(object_, "message"):
+            return object_.message.chat
+
+        msg = f"Can't find Chat in {type(object_).__name__} object."
+        raise RuntimeError(msg)
+
+    @staticmethod
+    def _check_type(chat_type: "Union[str, Set[str]]") -> None:
+        """Raise error if chat_type is not supported."""
+        if not isinstance(chat_type, (str, set)):
+            msg = (
+                f"Unsupported chat type: {type(chat_type).__name__}. "
+                f"Use `str` or `set` instead."
+            )
+            raise TypeError(msg)
