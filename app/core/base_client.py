4e0344d1377974e22b4d279844286ad650bb1b8a
Oleg A <t0rr@mail.ru>
2023-02-16 23:35:19 +0300

    chore: add logging

--- app/core/base_client.py
+++ app/core/base_client.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import ssl
 from typing import TYPE_CHECKING, Any, Optional, Union
 
@@ -18,6 +19,7 @@ class BaseClient:
     def __init__(self, base_url: "Union[str, URL]") -> None:
         self._base_url = base_url
         self._session: ClientSession | None = None
+        self.log = logging.getLogger(self.__class__.__name__)
 
     async def _get_session(self) -> ClientSession:
         """Get aiohttp session with cache."""
@@ -42,18 +44,39 @@ class BaseClient:
     ) -> tuple[int, dict[str, Any]]:
         """Make request and return decoded json response."""
         session = await self._get_session()
+
+        self.log.debug(
+            "Making request %r %r with json %r and params %r",
+            method,
+            url,
+            json,
+            params,
+        )
         async with session.request(method, url, params=params, json=json) as response:
-            return response.status, await response.json(loads=loads)
+            status = response.status
+            result = await response.json(loads=loads)
+
+        self.log.debug(
+            "Got response %r %r with status %r and json %r",
+            method,
+            url,
+            status,
+            result,
+        )
+        return status, result
 
     async def close(self) -> None:
         """Graceful session close."""
         if not self._session:
+            self.log.debug("There's not session to close.")
             return
 
         if self._session.closed:
+            self.log.debug("Session already closed.")
             return
 
         await self._session.close()
+        self.log.debug("Session successfully closed.")
 
         # Wait 250 ms for the underlying SSL connections to close
         # https://docs.aiohttp.org/en/stable/client_advanced.html#graceful-shutdown
