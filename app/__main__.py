b88e97ee4be179e458d971d87f5871370f688898
Oleg A <t0rr@mail.ru>
2023-09-25 10:52:31 +0300

    feat: enable uvloop

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,15 @@
 import logging
+from contextlib import suppress
 
 from aiohttp.web import run_app
 
 from app import app_factory
 from app.core.settings import WebAppSettings
+
+with suppress(ImportError):
+    import uvloop
+
+    uvloop.install()
 
 logging.basicConfig(level=logging.INFO)
 
